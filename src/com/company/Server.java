197177aead494cf510f99a0d87596c97c4cfa4d1
kourani55 <fakouran@umich.edu>
2022-02-01 17:18:23 -0500

    Update Server.java
    Created server and client sockets

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -13,6 +13,21 @@ public class Server {
     }//end main
 
     public static void createCommunicationLoop(){
+        try {
+
+            //creates server socket
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            
+            System.out.println("Server Started");
+
+            
+            //client socket 
+            Socket socket = serverSocket.accept();
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } //end try catch
+
 
     }//end func
 }
