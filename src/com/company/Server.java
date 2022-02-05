a8bfda7e5f7c7aa1d7a60796e1dd0ee01328a8ba
Ali Hazime <ahazi@umich.edu>
2022-02-04 19:40:35 -0500

    Unique users
    Users are now identifiable by username and password pairs.

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -26,28 +26,33 @@ public class Server {
 
             reader = new BufferedReader(new FileReader("logins.txt"));
             String line = reader.readLine();
-            List<String> list = new ArrayList<String>();
+            List<String> users = new ArrayList<String>();
+            List<String> passwords = new ArrayList<String>();
             String[] message;
             while (line != null) {
 
                 // lists user and pass
-                list.add(line.split(" ")[0]);
-                list.add(line.split(" ")[1]);
+                users.add(line.split(" ")[0]);
+                passwords.add(line.split(" ")[1]);
                 line = reader.readLine();
 
             }//end loop
             reader.close();
 
-            System.out.println(list.size());
+            System.out.println(users.size());
 
             //creates server socket
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
 
             System.out.println("Server Started");
 
+            boolean quit_flag;
+            boolean user_flag, pass_flag;
+            String currentUser=null;
+
             //keeps the server open for new clients
             while(!serverSocket.isClosed()){
-
+                quit_flag=false;
                 //client socket
                 System.out.println("Listening for new client...");
                 Socket socket = serverSocket.accept();
@@ -56,8 +61,7 @@ public class Server {
                 DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
                 DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
 
-                boolean quit_flag = false;
-                boolean user=false, pass=false;
+
                 //Initial communication loop
                 while(!quit_flag){
 
@@ -65,69 +69,69 @@ public class Server {
                     message = strReceived.split(" ",3);
 
                     if(message[0].equalsIgnoreCase("LOGIN")) {
-                        user = false;
-                        pass = false;
+                        user_flag = false;
+
                         if(message.length < 3){
                             System.out.println("Client failed login...");
                             outputToClient.writeUTF("Please provide a username and password when logging in.");
                         }
                         else{
-                            for(int i=0; i<list.size();i++){
-                                if(message[1].equalsIgnoreCase(list.get(i))){
-                                    user = true;
+                            for(int i=0; i<users.size();i++){
+                                if(message[1].equalsIgnoreCase(users.get(i)) && message[2].equalsIgnoreCase(passwords.get(i))){
+                                    user_flag = true;
+                                    currentUser = message[1];
                                 }
-                                if(message[2].equalsIgnoreCase(list.get(i))){
-                                    pass = true;
-                                }
-                                if(user && pass){
+
+                                if(user_flag){
                                     isLogged = true;
                                 }
                             }
                             if(isLogged) {
-                                System.out.println("Client is Logged On...");
-                                outputToClient.writeUTF("SUCCESS");
+                                System.out.println("Client "+ currentUser +" is Logged On...");
+                                outputToClient.writeUTF("SUCCESS: logged in as "+ currentUser);
                             }
                             else{
                                 System.out.println("Client failed login...");
                                 outputToClient.writeUTF("Incorrect login info, please try again.");
                             }
                         }
-                        //client must log in before accessing other commands
-                        while(isLogged == true) {
 
-                            strReceived = inputFromClient.readUTF();
-                            message = strReceived.split(" ",3);
-
-                            if(message[0].equalsIgnoreCase("LOGOUT")) {
-                                //disconnects client but keeps server running
-                                System.out.println("200 OK");
-                                isLogged = false;
-                                quit_flag=true;
-                                socket.close();
-                                break;
-                            }
-
-                            else if(message[0].equalsIgnoreCase("quit")) {
-                                System.out.println("Shutting down server...");
-                                outputToClient.writeUTF("Shutting down server");
-                                quit_flag = true;
-                                serverSocket.close();
-                                socket.close();
-                                break;
-                            }
-                            else if(message[0].equalsIgnoreCase("hello")){
-                                System.out.println("The client says hello");
-                                outputToClient.writeUTF("Hello client!");
-                            }
-                            else {
-                                System.out.println("Unknown command received:" + strReceived);
-                                outputToClient.writeUTF("Unknown command." + "Please try again.");
-                            }
-                        }
                     }//end login if statement
                     else {
                         System.out.println("Client not logged in");
                         outputToClient.writeUTF("Error." + "Please log in first.");
+                    }
+                    //client must log in before accessing other commands
+                    while(isLogged == true) {
+
+                        strReceived = inputFromClient.readUTF();
+                        message = strReceived.split(" ",3);
+
+                        if(message[0].equalsIgnoreCase("LOGOUT")) {
+                            //disconnects client but keeps server running
+                            System.out.println("200 OK");
+                            isLogged = false;
+                            quit_flag=true;
+                            socket.close();
+                            break;
+                        }
+
+                        else if(message[0].equalsIgnoreCase("quit")) {
+                            System.out.println("Shutting down server...");
+                            outputToClient.writeUTF("Shutting down server");
+                            quit_flag = true;
+                            serverSocket.close();
+                            socket.close();
+                            break;
+                        }
+                        else if(message[0].equalsIgnoreCase("hello")){
+                            System.out.println("The "+ currentUser +" says hello");
+                            outputToClient.writeUTF("Hello "+currentUser+"!");
+                        }
+                        else {
+                            System.out.println("Unknown command received:" + strReceived);
+                            outputToClient.writeUTF("Unknown command." + "Please try again.");
+                        }
                     }
 
                         /*(if(quit_flag){
